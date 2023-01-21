59ddea0e1e44c54e791caad33b7fcaaf8f6e922b
Şiyar Öner <siyaroner@gmail.com>
2023-01-21 15:22:03 +0300

    ImageTransofFarmat.py is done

--- ImageTransofrmations.py
+++ ImageTransofrmations.py
@@ -4,16 +4,46 @@ import numpy as np
 img=cv.imread("data/photos/kitten1.jpg")
 cv.imshow("Kitten",img)
 
-#Translation
-def translate(img,w,h):
-    transMat=np.float32([[1,0,w],[0,1,h]])
-    dimensions=(img.shape[1],img.shape[0])
-    return cv.warpAffine(img,transMat,dimensions)
-# -x -> left
-# -y -> up
-# x->right
-# y ->down
-translated=translate(img,100,200)
-cv.imshow("Translatedtranslate",translated)   
-cv.waitKey(0)
-# cv.destroyAllWindows()
+# #Translation
+# def translate(img,x,y):
+#     transMat=np.float32([[1,0,x],[0,1,y]])
+#     dimensions=(img.shape[1],img.shape[0])
+#     return cv.warpAffine(img,transMat,dimensions)
+# # -x -> left
+# # -y -> up
+# # x->right
+# # y ->down
+# # translated=translate(img,50,50)
+# translated=translate(img,-50,-50)
+
+# cv.imshow("Translatedtranslate",translated)   
+
+ 
+#  # Rotation
+ 
+# def rotate (img,angle,rotPoint=None):
+#     (height,width)=img.shape[:2]
+#     if rotPoint is None:
+#         rotPoint=(width//2,height//2)
+#     rotMat=cv.getRotationMatrix2D(rotPoint,angle,1.0)
+#     dimensions=(width,height)
+#     return cv.warpAffine(img,rotMat,dimensions)
+# rotated=rotate(img,180)
+# cv.imshow("Rotated",rotated)
+
+
+# ## Resizing
+
+# resized=cv.resize(img,(250,250),interpolation=cv.INTER_CUBIC)
+# cv.imshow("Resized",resized)
+
+
+# ## Flipping
+# flip=cv.flip(img,-1) #0: Vertically, 1:horizantally, -1:vertically and horizantally
+# cv.imshow("Flipped",flip)
+
+# cropping
+cropped=img[100:200,200:300]
+cv.imshow("Cropped",cropped)
+cv.waitKey( )
+cv.destroyAllWindows()
