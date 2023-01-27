bf8b4a62fafbffe4e7784de4ce341d95209e4b86
Şiyar Öner <siyaroner@gmail.com>
2023-01-27 22:16:27 +0300

    HistogramComputation.py is done but we habe a bug

--- HistogramComputation.py
+++ HistogramComputation.py
@@ -1,10 +1,37 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
 
 img=cv.imread("data/photos/ayasofyacami.jpg")
 cv.imshow("ayasofyacami",img)
 
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow("Gray",gray)
+blank=np.zeros(img.shape[:2],dtype="uint8")
+
+# gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# cv.imshow("Gray",gray)
+
+mask=cv.circle(blank,(img.shape[1]//2,img.shape[0]//2),100,255,-1)
+masked=cv.bitwise_and(img,img,mask=mask)
+cv.imshow("Mask",masked)
+
+# # Grayscale histogram
+# gray_hist=cv.calcHist([gray], [0],mask,[256],[0,256])
+# plt.figure()
+# plt.title("Grayscale Histogram")
+# plt.xlabel("Bins")
+# plt.ylabel("Number of pixels")
+# plt.plot(gray_hist)
+# plt.xlim(0,256)
+# plt.show()
+
+#Colour Histogram
+colors=("b","g","r")
+for i,col in enumerate(colors):
+    hist=cv.calcHist([img],[i],masked,[256],[0,256])
+    plt.plot(hist,color=col)
+    plt.xlim([0,256])
+    
+plt.show()
 
 cv.waitKey( )
 cv.destroyAllWindows()
