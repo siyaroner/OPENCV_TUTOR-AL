c5f93ba3b0f2c22595e2496045994d16579da179
Siyar Desktop <siyaroner@gmail.com>
2023-03-12 22:07:15 +0300

    Red light dedected in a traffic light

--- ObjectDetection&ObjectTrackingWithHSV.py
+++ ObjectDetection&ObjectTrackingWithHSV.py
@@ -2,15 +2,43 @@
 import cv2 as cv
 import numpy as np
 
-def nothing ():
+def nothing (x):
     pass
 
 cv.namedWindow("Tracking")
+cv.createTrackbar("LH","Tracking",0,255,nothing) #LH lower hue
+cv.createTrackbar("LS","Tracking",0,255,nothing) #lower saturation
+cv.createTrackbar("LV","Tracking",0,255,nothing) #lower value
+cv.createTrackbar("UH","Tracking",255,255,nothing) #upper hue
+cv.createTrackbar("US","Tracking",255,255,nothing) #upper saturation
+cv.createTrackbar("UV","Tracking",255,255,nothing) #upper value
+
 
 while True:
     frame =cv.imread("data/photos/trafficlight.jpg")
+    hsv=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+    # l_r=np.array([140,140,20])
+    # u_r=np.array([255,255,255])
+    
+    l_h=cv.getTrackbarPos("LH","Tracking")
+    l_s=cv.getTrackbarPos("LS","Tracking")
+    l_v=cv.getTrackbarPos("LV","Tracking")
+    
+    u_h=cv.getTrackbarPos("UH","Tracking")
+    u_s=cv.getTrackbarPos("US","Tracking")
+    u_v=cv.getTrackbarPos("UV","Tracking")
+        
+    l_r=np.array([l_h,l_s,l_v])
+    u_r=np.array([u_h,u_s,u_v])
+    print(l_r,u_r)
+    mask=cv.inRange(hsv,l_r,u_r)
+    res=cv.bitwise_and(frame,frame,mask=mask)
+    
     cv.imshow("frame",frame)
-    key=cv.waitKey( )
+    cv.imshow("mask",mask)
+    cv.imshow("res",res)
+    
+    key=cv.waitKey(1)
     if key==ord(" "):
         break
 cv.destroyAllWindows()
